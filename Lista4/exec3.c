30510df8057ec2af2ddc6a5fd33dbe7dfc1bb01e
Antoniocp63 <antcp63profissional@gmail.com>
2026-04-29 20:09:52 -0300

    exec3.c

---
+++ Lista4/exec3.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int i, numero, soma;
+   for (i = 1; i <= 5; i++) {
+       scanf("%d" , &numero);
+       if (numero > 0){
+           soma = soma + numero;
+       }
+   }
+    printf("Soma: %d\n", soma);
+    return 0;
+}
